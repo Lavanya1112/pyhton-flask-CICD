c5779a68c1d2323db25d5a1d1de2524ca50599c2
Lavanya1112 <78639731+Lavanya1112@users.noreply.github.com>
2022-10-31 12:56:03 +0530

    update app.py

--- flaskapp.py
+++ flaskapp.py
@@ -2,6 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
+
 def hello():
-    return '<h1>Hello, World!</h1>'
+        return "Welcome to Flask Application!"
+
+if __name__ == "__main__":
+
+        app.run(host='0.0.0.0',port=5000)
+
